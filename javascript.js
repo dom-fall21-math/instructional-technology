3045f017b75c1e6df624cb9c08f87fe114fb04fd
jambriz2 <ambrizjesus74@gmail.com>
2021-10-27 21:27:40 -0700

    Old

--- javascript.js
+++ javascript.js
@@ -1,10 +1,3 @@
-$(function() {
-  $('.show-button').click(function() {
-    $('.content-div').not('#btn' + $(this).attr('target')).hide();
-    $('#btn' + $(this).attr('target')).fadeToggle(300);
-  });
-});
-
 
 // GeoGebra Parameters for Graph 
 // Learn more about all of Parameters at: https://wiki.geogebra.org/en/Reference:GeoGebra_App_Parameters
@@ -74,4 +67,60 @@ applet1.setPreviewImage(
   'https://www.geogebra.org/images/GeoGebra_loading.png',
   'https://www.geogebra.org/images/applet_play.png'
 );
+
+$(function() {
+  $('.show-button').click(function() {
+    $('.content-div').not('#btn' + $(this).attr('target')).hide();
+    $('#btn' + $(this).attr('target')).fadeToggle(300);
+  });
+});
+
+  //  Use a closure to hide the local variable
+  //
+(function () {
+  var n = 1;
+
+  //
+  //  Make the current step be visible, and increment the step.
+  //  If it is the last step, disable the step button.
+  //  Once a step is taken, the reset button is made available.
+  //
+  window.ShowStep = function () {
+    document.getElementById("Set" + $(this).attr('target') + "-" + n++).style.visibility = "visible";
+    if (!document.getElementById("Set" + $(this).attr('target') + "-" + n)) {document.getElementById("step").disabled = true}
+    document.getElementById("reset").disabled = false;
+  }
+
+  //
+  //  Enable the step button and disable the reset button.
+  //  Hide the steps.
+  //
+  window.ResetSteps = function () {
+    document.getElementById("step").disabled = false;
+    document.getElementById("reset").disabled = true;
+    var i = 1, step; n = 1;
+    while (step = document.getElementById("Set" + $(this).attr('target') + "-" + i)) {step.style.visibility = "hidden"; i++}
+  }
+})();
+
+MathJax = {
+  tex: {inlineMath: [['$', '$'], ['\\(', '\\)']]},
+  chtml: {
+    displayAlign: 'left'
+  },
+  startup: {
+    ready: function () {
+      //
+      //  Do the usual startup (which does a typeset)
+      //
+      MathJax.startup.defaultReady();
+      //
+      //  When that is all done, un-hide the page
+      //
+      MathJax.startup.promise.then(function () {
+        document.getElementById("hidden").disabled = true;
+      });
+    }
+  }
+};
 
