db1c849768986a8da19cfa734b955787c4dfaedf
jambriz2 <ambrizjesus74@gmail.com>
2021-11-19 22:23:34 -0800

    Message

--- docs/scripts/javascript.js
+++ docs/scripts/javascript.js
@@ -1,21 +1,3 @@
-// Mathjax Paramatars
-  MathJax = {
-    tex: {inlineMath: [['$', '$'], ['\\(', '\\)']]},
-    chtml: {
-      displayAlign: 'left'
-    },
-    startup: {
-      pageReady: function () {
-        //
-        //  Do the usual startup (which does a typeset).
-        //  When that is all done, un-hide the page.
-        //
-        return MathJax.startup.defaultPageReady().then(function () {
-          document.getElementById("hidden").disabled = true;
-        });
-      }
-    }
-  };
 
 // Button Function 
 (function () {
